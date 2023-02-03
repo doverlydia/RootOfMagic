303c0a94388eac6a53efc6fe04c359fca47a4f98
Lydia Dover <doverlydia@gmail.com>
2023-02-03 11:18:42 +0200

    -

--- Assets/Code/Characters/IEffectable.cs
+++ Assets/Code/Characters/IEffectable.cs
@@ -9,18 +9,7 @@ namespace Characters
     {
         [field: SerializeField] public ReactiveCollection<StatusEffectData> CurrentStatusEffects { get; set; }
         public abstract UniTask ApplyEffect(StatusEffectData data);
-        public async UniTask DoEffect(StatusEffectData data)
-        {
-            CurrentStatusEffects.Add(data);
-            UniTask.RunOnThreadPool(async () =>
-            {
-                await UniTask.Delay((int)data.Duration * 1000);
-                RemoveEffect(data);
-                CurrentStatusEffects.Remove(data);
-            });
-            await ApplyEffect(data);
-        }
-
+        
         public void RemoveEffect(StatusEffectData data);
     }
 }
